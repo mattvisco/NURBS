22a1382f206af0f42bde22786bba6c3e6656ab38
Claire Tuna <tuna@Bjoern-Hartmanns-MacBook-Pro.local>
2013-11-07 13:57:38 -0800

    updated manifest

--- src/com/example/nowurbungholesmellgood/NURBS.java
+++ src/com/example/nowurbungholesmellgood/NURBS.java
@@ -1,15 +1,24 @@
 package com.example.nowurbungholesmellgood;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
-public class NURBS extends Activity {
 
+
+
+public class NURBS extends Activity {
+	private static final int REQUEST_ENABLE_BT=3;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_nurbs);
+		initializeBluetooth();
 	}
 
 	@Override
@@ -18,5 +27,18 @@ public class NURBS extends Activity {
 		getMenuInflater().inflate(R.menu.nurb, menu);
 		return true;
 	}
+BluetoothAdapter mBluetoothAdapter;
 
+	public void initializeBluetooth(){
+		// Initializes Bluetooth adapter.
+		final BluetoothManager bluetoothManager =
+		        (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+		mBluetoothAdapter = bluetoothManager.getAdapter();
+		
+		// displays a dialog requesting user permission to enable Bluetooth.
+		if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+	}
 }
